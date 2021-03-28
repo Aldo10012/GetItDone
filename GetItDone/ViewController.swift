244fe10f44e637e4a2911e424996b1ecc8fe62d1
Alberto <aldo10012@gmail.com>
2021-03-28 11:29:18 -0700

    removed storyboard

---
+++ GetItDone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetItDone
+//
+//  Created by Alberto Dominguez on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
