eddf106d466d8cc4f9887adc1b45705ccccb9e6a
Alberto <aldo10012@gmail.com>
2021-12-10 12:26:40 -0800

    added localNotifications

--- GetItDone/Controller/ToDoVC.swift
+++ GetItDone/Controller/ToDoVC.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import CoreData
+import UserNotifications
+
 
 class ToDoVC: UIViewController {
     
@@ -155,8 +157,45 @@ class ToDoVC: UIViewController {
             myData.createNewTodo(title: title, status: false, notes: notes, date: date)
         }
         
+        
+        setupLocalNotification()
+        
         self.dismiss(animated: true, completion: nil)
     }
+    
+    // MARK: Helpers
+    func setupLocalNotification() {
+        // Step 1: Ask for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+        }
+        
+        // Step 2: Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = titleTextField.text!
+        content.body = notesTextView.text
+        
+        // Step 3: Create the notification trigger
+        let date = datePicker.date
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // Step 4: Create the request
+        
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        // Step 5: Register the request
+        center.add(request) { (error) in
+            // Check the error parameter and handle any errors
+        }
+    }
+    
+    
     
     
     // MARK: UI
